cadcf27d559e0c3d37cd8257854d9929b78b6832
Rayco Haex <raycoh1299@gmail.com>
2022-04-28 22:00:15 +0200

    map orientation & menu

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,35 +1,50 @@
     <script>
+        import MapUI from './../components/MapUI.svelte'
+
         // Initialize and add the map
         function initMap() {
-        // The location of Uluru
-        const uluru = { lat: -25.344, lng: 131.031 };
-        // The map, centered at Uluru
-        const map = new google.maps.Map(document.getElementById("map"), {
-            disableDefaultUI: true,
-            zoom: 4,
-            center: {
-                lat: 51.4339328,
-                lng: 5.4669052
-            },
-            zoom: 17,
-            heading: 0,
-            tilt: 45,
-            mapId: "f4792ebdc438caa1"
-        });
-        // The marker, positioned at Uluru
-        const marker = new google.maps.Marker({
-            position: uluru,
-            map: map,
-        });
-        }
-        window.initMap = initMap;
+            const uluru = { lat: -25.344, lng: 131.031 };
+            // The map, centered at Uluru
+            const map = new google.maps.Map(document.getElementById("map"), {
+                disableDefaultUI: true,
+                zoom: 4,
+                center: {
+                    lat: 51.4339328,
+                    lng: 5.4669052
+                },
+                zoom: 17,
+                heading: 0,
+                tilt: 45,
+                mapId: "f4792ebdc438caa1"
+            });
+            // The marker, positioned at Uluru
+            const marker = new google.maps.Marker({
+                position: uluru,
+                map: map,
+            });
 
+        if (window.DeviceOrientationEvent) {
+            window.addEventListener("deviceorientation", function(event) {
+                handleOrientationEvent(rotateDegrees);
+            }, true);
+        }
+
+        let heading = 0;
+
+        var handleOrientationEvent = function(rotateDegrees) {
+            if(Math.abs(heading - rotateDegrees) > 8) {
+                heading = rotateDegrees;
+                map.setHeading(rotateDegrees);
+            }
+        };
+    }
+
+        window.initMap = initMap;
         
     </script>
     <!--The div element for the map -->
-    <div class="ui-layer">
-        
-    </div>
+    <MapUI/>
+
     <div class="map-wrapper">
         <div id="map"></div>
     </div>
